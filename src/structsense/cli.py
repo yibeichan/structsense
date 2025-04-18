887d697fa2b20c76323a5c7d9ba07af9b94fcb75
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-04-17 23:43:25 -0400

    human in loop and run until options added

--- src/structsense/cli.py
+++ src/structsense/cli.py
@@ -35,13 +35,12 @@ def cli(ctx):
     type=str,
     help=("Path to the embedding configuration in YAML format or or dictionary"),
 )
-
 @click.option(
     "--knowledgeconfig",
     required=False,
     type=str,
     help=(
-        "Path to the configuration in YAML format or or dictionary that specify the search knowledge search key."
+            "Path to the configuration in YAML format or or dictionary that specify the search knowledge search key."
     ),
 )
 @click.option(
@@ -49,22 +48,32 @@ def cli(ctx):
     required=True,
     help=("The source—whether a file (text or PDF), a folder, or a text string."),
 )
+
 @click.option(
-    "--humaninloop",
+    "--enable_human_feedback",
     required=False,
     help=("Option to indicate whether to enable human in the loop, default True"),
 )
 @click.option(
     "--agent_feedback_config",
     required=False,
-    help=("Option provide the configuration that defines whether to enable human loop for the agents, e.g., extractor agent."
-          "By default, human loop for other agents is disabled except for HumanFeedbackAgent."
-          "Note: The option humaninloop should be set to True for this to work."),
+    help=(
+    "Option provide the configuration that defines whether to enable human loop for the agents, e.g., extractor agent."
+    "By default, human loop for other agents is disabled except for HumanFeedbackAgent."
+    "Note: The option humaninloop should be set to True for this to work."),
 )
-
+@click.option(
+    "--run_until_step",
+    required=False,
+    help=("Optional step that allows you to run agent partially. For example you may want to run just extractor agent."
+          "Options:"
+          "extracted_structured_information: Extractor agent"
+          "align_structured_information: Run until alignment agent"
+          "judge_alignment: Run until judge agent"),
+)
 def extract(
-    agentconfig, taskconfig, embedderconfig, knowledgeconfig, source,
-        humaninloop, agent_feedback_config
+        agentconfig, taskconfig, embedderconfig, knowledgeconfig, source,
+        enable_human_feedback, agent_feedback_config, run_until_step
 ):
     """Extract the terms along with sentence."""
     logger.info(
@@ -79,15 +88,16 @@ def extract(
         embedderconfig=embedderconfig,
         knowledgeconfig=knowledgeconfig,
         input_source=source,
-        enable_human_feedback=humaninloop,
-        agent_feedback_config=agent_feedback_config
+        enable_human_feedback=enable_human_feedback,
+        agent_feedback_config=agent_feedback_config,
+        run_until_step=run_until_step
 
     )
 
-    click.echo("*"*100)
+    click.echo("*" * 100)
     click.echo("Result")
     click.echo(result)
-    click.echo("*"*100)
+    click.echo("*" * 100)
 
 
 if __name__ == "__main__":
